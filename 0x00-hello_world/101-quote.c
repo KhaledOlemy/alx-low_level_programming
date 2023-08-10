ccccefe6a7d707c3d86b8ff2792f7ada4ab66798
khaledolemy <khaledolemy@gmail.com>
2023-08-10 19:52:55 +0300

    101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main: this will print without the use printf, puts
+ * 
+ * Return: 1, always working
+ */
+int main(void)
+{
+  write(1,'and that piece of art is useful" - Dora Korpar, 2015-10-19', 100);
+  return (0);
+}
