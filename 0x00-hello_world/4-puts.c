a97587b58a185a3cc94e09953d0db444c25eb814
khaledolemy <khaledolemy@gmail.com>
2023-08-10 18:27:58 +0300

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-int main(void)
+int main(void) //this is main
 {
   /* this is my first C program that prints out */
 puts("\"Programming is like building a multilingual puzzle");
