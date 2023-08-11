4b79c3129cbf6bfd49742f1829db6b35ce9773f2
khaledolemy <khaledolemy@gmail.com>
2023-08-11 18:55:38 +0300

    fixed

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main- this is for printing Alphabet
+ *
+ * Return: 0, always success
+ */
+
+int main(void)
+{
+	int i, j, x, y;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = i + 1; j < 10; j++)
+		{
+			putchar(i + 48);
+			putchar(j + 48);
+			if (i != 8)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	return (0);
+}
