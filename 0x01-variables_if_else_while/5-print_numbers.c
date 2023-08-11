7e0e774d1530be5e7cd54093f9a28894583f0edc
khaledolemy <khaledolemy@gmail.com>
2023-08-11 17:14:45 +0300

    fixed

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main- this prints numbers from 0 to 9 followed by /n
+ *
+ * Return: 0, always success
+ */
+int main(void)
+{
+	int x;
+
+	x = 0;
+	while (x < 10)
+	{
+		printf("%d", x);
+		x++ 1;
+	}
+	putchar('\n');
+	return (0);
+}
