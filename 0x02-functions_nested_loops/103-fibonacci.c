9f63e852d0a721e15719e49c16a55ff368750db6
khaledolemy <khaledolemy@gmail.com>
2023-08-15 21:41:50 +0300

    fixed

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -6,7 +6,7 @@
 */
 int main(void)
 {
-	long y, z, m;
+	long y, z, m, out;
 
 	y = 1;
 	z = 2;
@@ -17,10 +17,11 @@ int main(void)
 		z += m;
 		if (z >= 4000000)
 		{
+			out = z;
 			break;
 		}
 	}
-	printf("%lu", z);
+	printf("%lu", out);
 	putchar('\n');
 	return (0);
 }
