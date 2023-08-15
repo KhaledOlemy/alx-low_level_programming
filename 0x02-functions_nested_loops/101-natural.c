3c95a0c50286652dfc142937d8fe95aff6c61115
khaledolemy <khaledolemy@gmail.com>
2023-08-15 20:30:43 +0300

    fixed

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main- this prints numbers under 1024, that are divisible
+ * by 3 or f
+ *
+ * Return: 0 Success
+ */
+int main(void)
+{
+	int sum = 0;
+	int i,x,y;
+	for (i = 0; i < 1024; i++)
+	{
+		x = i % 3;
+		y = i % 5;
+		if (x == 0 || y == 0)
+		{
+			sum = sum + i;
+		}
+	}
+	printf("%d", sum);
+	putchar('\n');
+	return (0);
+}
