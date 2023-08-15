55d508202c7e0a84aab43793f51bbc2a8c4a7a26
khaledolemy <khaledolemy@gmail.com>
2023-08-15 21:37:02 +0300

    fixed

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,34 @@
+# include <stdio.h>
+/**
+ * main - print first 50 fibonacci number
+ *
+ * Return: 0 - Success
+*/
+int main(void)
+{
+	long x, y, z, m;
+
+	y = 1;
+	z = 2;
+	putchar(48 + 1);
+	putchar(',');
+	putchar(' ');
+	while (z < 4000000)
+	{
+		printf("%lu", z);
+		m = y;
+		y = z;
+		z += m;
+		if (z >= 4000000)
+		{
+			break;
+		}
+		else
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
