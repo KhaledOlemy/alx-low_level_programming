8e65cb39b2644fec9b787471dac50aa954b556d7
khaledolemy <khaledolemy@gmail.com>
2023-08-15 15:49:11 +0300

    fixed

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main- this prints the word _putchar
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int i;
+	char *val = "_putchar";
+
+	for (i = 0; i < strlen(val); i++)
+	{
+		putchar(val[i]);
+	}
+	putchar('\n');
+	return (0);
+}
