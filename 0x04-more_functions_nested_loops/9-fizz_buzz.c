2f1ce46d4f697cfb77a015fe8dbf729d032044ac
khaledolemy <khaledolemy@gmail.com>
2023-08-17 14:26:10 +0300

    fixed

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ * main- this functions represents the FIZZ BUZZ problem
+ * if a number is divisible by 3, replace it with Fizz
+ * if a number is divisible by 5, replace it with Buzz
+ * if a number is divisible by both 3 and 5, replace it with FizzBuzz
+ * else: print the number itself
+ * Return: 0, always success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+		printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
