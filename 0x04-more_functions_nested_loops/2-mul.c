5d29a1d2fb048ea965f22f259aaf10e992584e6f
khaledolemy <khaledolemy@gmail.com>
2023-08-17 13:02:45 +0300

    fixed

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+/**
+ * mul- this function multiplies (yes just line *)
+ * @a: first input
+ * @b: second input
+ *
+ * Return: out, the multiplication output
+*/
+int mul(int a, int b)
+{
+	int out = a * b;
+
+	return (out);
+}
