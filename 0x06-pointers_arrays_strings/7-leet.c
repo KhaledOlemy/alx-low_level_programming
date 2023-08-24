87353af32a81b2c82babdfb93ba662947898fc03
khaledolemy <khaledolemy@gmail.com>
2023-08-24 03:18:50 +0300

    fixed

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,30 @@
+/**
+ * leet- this function converts text into leet style
+ * a e o t l A E O T L - these letters are replaced wiht 4 3 0 7 1
+ * @s: this is the input string
+ * Return: leet-styled string.
+*/
+char *leet(char *s)
+{
+	int i, j;
+	char *s1 = "aeotlAEOTL";
+	char *s2 = "4307143071";
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == '\0')
+		{
+			break;
+		}
+		for (j = 0; j < 10; j++)
+		{
+			if (s[i] == s1[j])
+			{
+				s[i] = s2[j];
+			}
+		}
+		i++;
+	}
+	return (s);
+}
