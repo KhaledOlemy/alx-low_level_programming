4ddc8411d372025b69bc294e48c9765b4957cf4f
khaledolemy <khaledolemy@gmail.com>
2023-08-28 19:20:39 +0300

    fixed

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+/**
+ * set_string- this function replaces things
+ * @s: this is the string to be replaces
+ * @to: this is the string to be copied from
+*/
+void set_string(char **s, char *to)
+{
+	while (*to != '\0')
+	{
+		*s = to;
+		s += 1;
+		to += 1;
+	}
+}
