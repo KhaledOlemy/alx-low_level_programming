1c62d5a7d8ba48d7e99dc0bf5d258f49632b19ff
khaledolemy <khaledolemy@gmail.com>
2023-08-28 14:05:09 +0300

    fixed

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+/**
+ * _memset- this function sets the following n bytes with
+ * char b
+ * @s: this is the address of the start of the addressing
+ * @b: this is the byte to be assigned to the addresses
+ * @n: this is the count of addresses to be assigned to.
+ * Return: the address of the last byte assigned
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		*(s + i) = b;
+	}
+	return (s + i);
+}
