352820258b192b4ff657366523941a9d8c06ef01
khaledolemy <khaledolemy@gmail.com>
2023-08-30 14:45:14 +0300

    palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,3 +1,19 @@
+#include "main.h"
+
+/**
+ * is_palindrome- this is a function to determine if a string
+ * is palindrome or not
+ * @s: pointer to the beginning of the string
+ * Return: 1 if palindrome, 0 if not
+ */
+int is_palindrome(char *s)
+{
+	int x;
+
+	x = str_len(s, 0);
+	return (is_it_pali(s, x, 0));
+}
+
 /**
  * is_it_pali- this is a sub-function to determine if a string
  * is palindrome or not
@@ -18,22 +34,23 @@ int is_it_pali(char *s, int len, int index)
 	}
 	return (is_it_pali(s, len, index + 1));
 }
-/**
- * is_palindrome- this is a function to determine if a string
- * is palindrome or not
- * @s: pointer to the beginning of the string
- * Return: 1 if palindrome, 0 if not
- */
-int is_palindrome(char *s)
-{
-	char *ptr;
-	int x;
 
-	ptr = s;
-	while (*ptr != '\0')
+/**
+ * str_len- this is a sub-function to calculate string length
+ * @s: pointer to the beginning of the string
+ * @l: initial length
+ * Return: string length
+ */
+
+int str_len(char *s, int l)
+{
+	if (*s != '\0')
 	{
-		ptr += 1;
+		l += 1;
 	}
-	x = ptr - s;
-	return (is_it_pali(s, x, 0));
+	if (*s == '\0')
+	{
+		return (l);
+	}
+	return (str_len((s + 1), l));
 }
