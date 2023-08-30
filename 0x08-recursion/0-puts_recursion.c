e1877e3753b3dfd8b2bcde7a93d8cc587694c2fc
khaledolemy <khaledolemy@gmail.com>
2023-08-30 10:37:29 +0300

    fixed

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts_recursion- this function prints strings like puts
+ * but we used recursion with putchar
+ * @s: pointer to the start of the string
+*/
+void _puts_recursion(char *s)
+{
+	if (s[0] == '\0')
+	{
+		return;
+	}
+	_putchar(s[0]);
+	_puts_recursion(s + 1);
+}
