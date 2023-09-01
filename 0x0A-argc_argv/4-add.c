a6ffa455fb3785ef2772efcb7a26f6f5a30d314f
khaledolemy <khaledolemy@gmail.com>
2023-09-01 19:33:27 +0300

    fixed

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main-this function adds number together
+ * @argc: number of arguments entered
+ * @argv: actual arguments entered
+ * Return: 0 if success, 1 if literal entered
+ */
+int main(int argc, char *argv[])
+{
+	int i, sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		if (atoi(argv[i]) == 0 && *argv[i] != 48)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	printf("%d\n", atoi("a"));
+	return (0);
+}
