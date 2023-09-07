ddada97095139e0b5d64fb171b6db332394bd02c
khaledolemy <khaledolemy@gmail.com>
2023-09-07 17:41:27 +0300

    1_string_noncat_fixed

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -35,7 +35,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		n = (unsigned int) l2;
 	}
-	target = malloc(l1 + n);
+	target = malloc(l1 + n + 1);
 	if (target == NULL)
 	{
 		return (NULL);
