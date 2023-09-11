b79f8d31a767eb3df00436102c9b2a4a588f66fb
khaledolemy <khaledolemy@gmail.com>
2023-09-11 21:58:45 +0300

    first struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef _dog_h_
+#define _dog_h_
+/**
+ * struct dog - this is dog of struct type
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
