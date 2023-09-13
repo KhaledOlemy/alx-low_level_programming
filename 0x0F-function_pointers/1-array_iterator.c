be932ffbae7d81249a8ec25c6c189e6dfb708e81
khaledolemy <khaledolemy@gmail.com>
2023-09-13 18:41:27 +0300

    array iterator and print styles

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * array_iterator - prints array based on the style
+ * of the action print style
+ * @array: array to be printed
+ * @size: size of the array
+ * @action: pointer to print function to get the print style from.
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	for (i = 0; i < (int) size; i++)
+	{
+		action(array[i]);
+	}
+}
