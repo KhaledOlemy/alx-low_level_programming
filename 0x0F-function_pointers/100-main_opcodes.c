a38f5003fba36db02a848d13223d46c74d270088
khaledolemy <khaledolemy@gmail.com>
2023-09-13 23:35:23 +0300

    bonus hopefully done

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints out opcodes of its own function
+ * @argc: number of arguments opted in
+ * @argv: actual arguments opted in
+ * Return:	0 if everything is fine
+ *		1 if wrong number of arguments entered
+ *		2 if number entered is less than 0
+ */
+int main(int argc, char *argv[])
+{
+	int number_of_bytes, i;
+	char *array;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	number_of_bytes = atoi(argv[1]);
+	if (number_of_bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	array = (char *)main;
+	for (i = 0; i < number_of_bytes; i++)
+	{
+		printf("%02hhx", array[i]);
+		if (i != number_of_bytes - 1)
+		{
+			printf(" ");
+		}
+		else
+		{
+			printf("\n");
+			break;
+		}
+	}
+	return (0);
+}
