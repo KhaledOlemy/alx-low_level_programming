22332e7254fd562fe1dcfb3b7ffa3d866f4dab64
khaledolemy <khaledolemy@gmail.com>
2023-09-13 18:43:12 +0300

    array iterator and print styles

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -8,12 +8,11 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	if (!array || !size || !action)
+	int i;
+	if (!array || !size)
 	{
 		return;
 	}
-	int i;
-
 	for (i = 0; i < (int) size; i++)
 	{
 		action(array[i]);
