051da9e57456e59778e1111b5c4ed9a8c618dde8
khaledolemy <khaledolemy@gmail.com>
2023-09-13 18:01:57 +0300

    print name different styles

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,10 @@
+/**
+ * print_name - prints a word depends on the function entered
+ * as an argument
+ * @name: the input name to be printed
+ * @f: pointer to function, styled as the function
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
