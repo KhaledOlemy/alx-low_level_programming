aba931b2a990bcfc647513e590c421f5563b7f37
khaledolemy <khaledolemy@gmail.com>
2023-09-24 17:29:52 +0300

    0th fixed

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -108,15 +108,3 @@ int _print_int(unsigned int num, unsigned int iter, int flag)
 	}
 	return (i);
 }
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
