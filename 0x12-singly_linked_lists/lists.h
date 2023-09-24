0ac22f3d001d2380206a6867535ebf1f437e1b38
khaledolemy <khaledolemy@gmail.com>
2023-09-24 19:48:11 +0300

    first 'bonus' done

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -23,4 +23,5 @@ list_t *add_node(list_t **head, const char *str);
 char *_strdup(char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+void __attribute__((constructor)) print_first(void);
 #endif
