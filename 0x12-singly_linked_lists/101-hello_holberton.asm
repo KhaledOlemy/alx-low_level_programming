285f2e7489f386b62868dc7d0dcae0c748140b23
khaledolemy <khaledolemy@gmail.com>
2023-09-24 20:12:40 +0300

    second bonus hopefully done

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,11 +1,11 @@
 	extern printf
-message:		db "Hello, Holberton\n", 0
+message:		db "Hello, Holberton", 0
 format:			db "%s", 10, 0
 	global main
 main:
 	push	rbp
-	mov	rsi,format
-	mov	rdi,message
+	mov	rdi,format
+	mov	rsi,message
 	call	printf
 	pop	rbp
 	ret
