558222cfcf6bf195647f78a7612fcae10c39f981
khaledolemy <khaledolemy@gmail.com>
2023-09-25 20:50:00 +0300

    free list 2 && header

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_listint2 - free singly linked list and assign head to NULL
+ * @head: head of the list
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *holder, *temp;
+
+	holder = *head;
+	while (holder)
+	{
+		temp = holder->next;
+		free(holder);
+		holder = temp;
+	}
+	*head = NULL;
+}
