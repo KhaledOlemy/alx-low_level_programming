c61cc363d187a3acd05d38cc008953ec7656aa2d
khaledolemy <khaledolemy@gmail.com>
2023-09-26 18:25:32 +0300

    free (safe) list && header

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+/**
+ * free_listint_safe - frees (safe version) singly linked list
+ * and sets head to NULL after done
+ * @h: head of the list
+ *
+ * Return: len of the list
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	listint_t *temp;
+	size_t len = 0;
+	int is_valid;
+
+	while ((*h)->next)
+	{
+		is_valid = (*h) - (*h)->next;
+		len += 1;
+		if (is_valid > 0)
+		{
+			temp = (*h)->next;
+			free(*h);
+			*h = temp;
+		}
+		else
+		{
+			break;
+		}
+	}
+	free(*h);
+	*h = NULL;
+	return (len);
+}
