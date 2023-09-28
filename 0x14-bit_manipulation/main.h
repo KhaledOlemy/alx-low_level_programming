11cca58c0fc0df97e8bcf8581b57ddc8cd38f570
khaledolemy <khaledolemy@gmail.com>
2023-09-28 16:04:49 +0300

    print decimal in binary

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -5,4 +5,7 @@
 #include <stdlib.h>
 unsigned int binary_to_uint(const char *b);
 int _pow_recursion(int x, int y);
+int _putchar(char c);
+void print_binary(unsigned long int n);
+void _print_binary(unsigned int num, unsigned int iter, int flag);
 #endif
