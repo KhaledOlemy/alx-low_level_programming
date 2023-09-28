f596fd908e9b1e18553e2ca9cc25e820ef163d59
khaledolemy <khaledolemy@gmail.com>
2023-09-28 19:51:36 +0300

    alter bit at index

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * set_bit - sets a bit at @index to 1
+ * @n: number to alter
+ * @index: index, at which to set to 1
+ *
+ * Return: altered number
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= 64)
+	{
+		return (-1);
+	}
+	if (*n & _pow_recursion(2, index))
+	{
+		return (*n);
+	}
+	return (*n += _pow_recursion(2, index));
+}
+
+/**
+ * _pow_recursion- this function calculates x to the power of y
+ * @x: the base number
+ * @y: the exponent
+ * Return: x to the power of y if y >= 0; else -1 to indicate error.
+*/
+int _pow_recursion(unsigned long int x, unsigned long int y)
+{
+	if (y == 0)
+	{
+		return (1);
+	}
+	return (x * _pow_recursion(x, y - 1));
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -8,4 +8,5 @@ int _putchar(char c);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int _pow_recursion(unsigned long int x, unsigned long int y);
+int set_bit(unsigned long int *n, unsigned int index);
 #endif
