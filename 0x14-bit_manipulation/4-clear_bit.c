4147eebc9a09a6f458404e393d962064b007f6d0
khaledolemy <khaledolemy@gmail.com>
2023-09-28 20:01:21 +0300

    clear bit at index

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * clear_bit - sets a bit at @index to 0
+ * @n: number to alter
+ * @index: index, at which to set to 0
+ *
+ * Return: 1 if it worked, 0 if not
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (!*n || (!index && index != 0) || index >= 64)
+	{
+		return (-1);
+	}
+	if (*n & _pow_recursion(2, index))
+	{
+		*n -= _pow_recursion(2, index);
+		return (1);
+	}
+	return (1);
+}
+
+
+/**
+ * _pow_recursion- this function calculates x to the power of y
+ * @x: the base number
+ * @y: the exponent
+ * Return: x to the power of y if y >= 0; else -1 to indicate error.
+*/
+int _pow_recursion(unsigned long int x, unsigned long int y)
+{
+	if (y == 0)
+	{
+		return (1);
+	}
+	return (x * _pow_recursion(x, y - 1));
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -9,4 +9,5 @@ void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int _pow_recursion(unsigned long int x, unsigned long int y);
 int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
 #endif
