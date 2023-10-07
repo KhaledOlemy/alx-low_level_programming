b8e232fa6892aee519a9e34e0100444fe2aa8394
khaledolemy <khaledolemy@gmail.com>
2023-10-07 14:08:25 +0300

    append to file

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * append_text_to_file - appends @text_content to a @filename
+ * @filename: file to which append
+ * @text_content: content to be appended
+ *
+ * Return:	1 if everything is okay
+ *		-1 if something fails
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int o, c, w;
+
+	o = open(filename, O_RDONLY);
+	if (o == -1 || !filename)
+	{
+		return (-1);
+	}
+	c = open(filename, O_APPEND | O_WRONLY);
+	if (c == -1)
+	{
+		return (-1);
+	}
+	if (text_content)
+	{
+		w = write(c, text_content, strlen(text_content) - 1);
+	}
+	if (w == -1)
+	{
+		return (-1);
+	}
+	return (1);
+}
