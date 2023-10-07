ac42ebf39f00e2d9f1cfd6b4c4e5d3e4de343895
khaledolemy <khaledolemy@gmail.com>
2023-10-07 14:14:06 +0300

    append to file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -27,7 +27,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	}
 	if (text_content)
 	{
-		w = write(c, text_content, strlen(text_content) - 1);
+		w = write(c, text_content, strlen(text_content));
 	}
 	if (w == -1)
 	{
