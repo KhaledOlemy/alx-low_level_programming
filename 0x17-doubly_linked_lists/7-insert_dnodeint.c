62513926df2fb6aff1c63fd54c71ca54e897df6f
khaledolemy <khaledolemy@gmail.com>
2023-11-20 23:16:26 +0200

    insert at index fix#4 rev

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -45,16 +45,5 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		holder->prev = new_node;
 		return (new_node);
 	}
-	if (idx == i + 1)
-	{
-		new_node = malloc(sizeof(dlistint_t));
-		if (!new_node)
-		{return (NULL); }
-		new_node->n = n;
-		new_node->prev = holder;
-		new_node->next = NULL;
-		holder->prev->next = new_node;
-		return (new_node);
-	}
 	return (NULL);
 }
