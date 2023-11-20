70e567ede6cee94eee5175ebb9a9629db65f5896
khaledolemy <khaledolemy@gmail.com>
2023-11-20 22:52:42 +0200

    insert at index fix

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -19,6 +19,16 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			holder = holder->prev;
 		}
 	}
+	else
+	{
+		new_node = malloc(sizeof(dlistint_t));
+		if (!new_node)
+		{return (NULL); }
+		new_node->n = n;
+		new_node->next = NULL;
+		new_node->prev = NULL;
+		*h = new_node;
+	}
 	while (holder && (i != idx))
 	{
 		holder = holder->next;
