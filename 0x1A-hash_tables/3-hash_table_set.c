c85f11aa5d0a42bf061cfbda598704c931461435
khaledolemy <khaledolemy@gmail.com>
2023-12-13 06:14:47 +0200

    table set v1

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,26 +14,27 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 
 	if (!ht || !key || *key == '\0' || !value)
-	{
-		return (0);
-	}
+	{return (0); }
 	val_cpy = strdup(value);
 	if (!val_cpy)
-	{
-		return (0);
-	}
+	{return (0); }
 	new_node = malloc(sizeof(hash_node_t));
 	if (!new_node)
-	{
-		return (0);
-	}
+	{return (0); }
 	new_node->key = strdup(key);
 	if (!new_node->key)
+	{return (0); }
+	index = key_index((const unsigned char *)key, ht->size);
+	if (ht->array[index])
 	{
-		return (0);
+		if (strcmp(ht->array[index]->key, new_node->key) == 0)
+		{
+			free(ht->array[index]->value);
+			ht->array[index]->key = val_cpy;
+			return (1);
+		}
 	}
 	new_node->value = val_cpy;
-	index = key_index((const unsigned char *)key, ht->size);
 	target_node = ht->array[index];
 	if (target_node)
 	{
