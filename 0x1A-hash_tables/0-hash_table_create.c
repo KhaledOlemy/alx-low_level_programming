886a62ab4bf338edea98c8a376635aecd49108eb
khaledolemy <khaledolemy@gmail.com>
2023-12-13 03:19:26 +0200

    1st C file in a month fix + .h

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -16,8 +16,8 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 	}
 	hash_table->size = size;
-	hash_table->array = malloc(sizeof(hash_node_t *) * size);
-	if (!(hash_table->array))
+	hash_table->array = malloc(sizeof(hash_node_t) * size);
+	if (!hash_table->array)
 	{
 		return (NULL);
 	}
