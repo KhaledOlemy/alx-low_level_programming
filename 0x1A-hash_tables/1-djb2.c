c5f102418646939ad41fbb25d05d6a86d76d2e2e
khaledolemy <khaledolemy@gmail.com>
2023-12-13 03:26:46 +0200

    hash func, nth to do

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,12 +1,18 @@
+/**
+ * hash_djb2 - hash function using magic 33
+ * @str: string to be hashed
+ *
+ * Return: hashing code
+ */
 unsigned long int hash_djb2(const unsigned char *str)
 {
-    unsigned long int hash;
-    int c;
+	unsigned long int hash;
+	int c;
 
-    hash = 5381;
-    while ((c = *str++))
-    {
-        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-    }
-    return (hash);
+	hash = 5381;
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
+	return (hash);
 }
