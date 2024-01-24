165a95398f59db5137de5a66e32ef0b084aa1a7c
khaledolemy <khaledolemy@gmail.com>
2024-01-25 00:39:26 +0200

    pythoned

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -10,8 +10,10 @@ def island_perimeter(grid=[[]]):
     Return: perimeter of land
     """
     len_1 = len(grid)
+    if len_1 <= 1:
+        return 0
     len_2 = len(grid[0])
-    if len_1 == 0 or len_2 == 0:
+    if len_2 <= 1:
         return 0
     perimeter = 0
     for i in range(len_1):
@@ -28,3 +30,4 @@ def island_perimeter(grid=[[]]):
                     pin += grid[i+1][j]
                 perimeter = perimeter + (4-pin)
     return perimeter
+
